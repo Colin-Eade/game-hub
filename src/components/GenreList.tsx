5ae704b8bcd433d7a0050be971a03ba9e469c15d
Colin Eade <colin.eade@dcmail.ca>
2024-04-29 15:58:14 -0400

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,11 @@
-import useGenres from '../hooks/useGenres';
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
-import getCroppedImageUrl from '../services/image-url';
+import useGenres, { Genre } from '../hooks/useGenres';
+import { Button, List, ListItem, Spinner } from '@chakra-ui/react';
 
-const GenreList = () => {
+interface Props {
+    onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
     const { data, isLoading, error } = useGenres();
 
     if (error) return null;
@@ -12,14 +15,13 @@ const GenreList = () => {
         <List>
             {data.map((genre) => (
                 <ListItem key={genre.id} paddingY="5px">
-                    <HStack>
-                        <Image
-                            boxSize="32px"
-                            borderRadius={8}
-                            src={getCroppedImageUrl(genre.image_background)}
-                        />
-                        <Text fontSize="lg">{genre.name}</Text>
-                    </HStack>
+                    <Button
+                        onClick={() => onSelectGenre(genre)}
+                        fontSize="lg"
+                        variant="link"
+                    >
+                        {genre.name}
+                    </Button>
                 </ListItem>
             ))}
         </List>
