65589ed989b23b3201d20e55a3cca63fe3512137
Colin Eade <colin.eade@dcmail.ca>
2024-05-03 14:52:49 -0400

    Refactor: entity interface

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import ApiClient from '../services/api-client';
 import ms from 'ms';
-import { Genre } from '../entities/Genre';
+import Genre from '../entities/Genre';
 
 const apiClient = new ApiClient<Genre>('/genres');
 
