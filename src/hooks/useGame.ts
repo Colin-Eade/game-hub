22824100108560d254ec5042bef73d06ba2738f9
Colin Eade <colin.eade@dcmail.ca>
2024-05-03 12:48:01 -0400

    Refactor: move entity interfaces

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import ApiClient from '../services/api-client';
-import { Game } from './useGames';
+
+import { Game } from '../entities/Game';
 
 const apiClient = new ApiClient<Game>('/games');
 
